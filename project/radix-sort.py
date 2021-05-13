a2dc6adbab520bcd1834d2e351a911b7bffca71f
Charles Hart <chart7@hawk.iit.edu>
2021-05-12 20:59:59 -0500

    completed project

--- project/radix-sort.py
+++ project/radix-sort.py
@@ -1,5 +1,5 @@
 import urllib
-import requests
+import urllib.request
 
 def book_to_words(book_url='https://www.gutenberg.org/files/84/84-0.txt'):
     booktxt = urllib.request.urlopen(book_url).read().decode()
@@ -7,4 +7,30 @@ def book_to_words(book_url='https://www.gutenberg.org/files/84/84-0.txt'):
     return bookascii.split()
 
 def radix_a_book(book_url='https://www.gutenberg.org/files/84/84-0.txt'):
-    pass
+    lst = book_to_words()
+    largestlen = len(max(lst,key = len)) - 2
+    rSorted = countSort(lst,largestlen + 1)
+    for i in range(largestlen, -1, -1):
+        rSorted = countSort(rSorted, i)
+    return rSorted
+
+def countSort(lst, digit):
+    c = [0] * 128
+    s = [None] * len(lst)
+    for i in lst:
+        if len(i) - 1 < digit:
+            k = 0
+        else:
+            k = ord(i.decode('ascii')[digit])
+        c[k] += 1
+    for i in range(1, len(c)):
+        c[i] += c[i - 1]
+    for i in range(len(lst) - 1, -1, -1):
+        if len(lst[i]) - 1 < digit:
+            k = 0
+        else:
+            k = ord(lst[i].decode('ascii')[digit])
+        s[c[k] - 1] = lst[i]
+        c[k] += -1
+    return s
+print(radix_a_book())
